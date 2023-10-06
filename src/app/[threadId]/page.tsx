0904b1a159e5df7cccd7677b85575767c1876caf
mori8 <kaithape@gmail.com>
2023-10-06 15:23:42 +0900

    improvement: auto-height based on text length

---
+++ src/app/[threadId]/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import ChatBox from "../components/ChatBox";
+import ModuleGroupBox from "./ModuleGroupBox";
+
+type Props = {
+  params: {
+    threadId: string;
+  };
+};
+
+export default function page({ params }: Props) {
+  const [threadId, setThreadId] = useState<string | null>(null);
+  const [message, setMessage] = useState<
+    { messageId: number; message: string }[]
+    >([]);
+  const [modules, setModules] = useState<Module[]>([]);
+  // 난 무섭다 데이터가 어떻게 생겼을지 가늠도 안된다
+
+  useEffect(() => {
+    setThreadId(params.threadId);
+  }, [params.threadId]);
+
+  const editMessage = (messageId: number, text: string) => {
+    // TODO: fetch edited query to server
+
+    // TODO: (then) update message state
+    setMessage((prev) => {
+      const index = prev.findIndex(
+        (message) => message.messageId === messageId
+      );
+      if (index !== -1) {
+        const newMessage = [...prev];
+        newMessage[index].message = text;
+        return newMessage;
+      }
+      return prev;
+    });
+  };
+
+  useEffect(() => {
+    if (threadId) {
+      // TODO: fetch thread
+      setMessage([
+        {
+          messageId: 1,
+          message: `Based on this diagnostic history, give your opinion on what prescription you would give this patient.
+
+2021-03 Visual hallucination
+수면과 관계없이 밤낮으로 귀신이나 괴물이 보인다
+눈을 감으면 귀신이 보이고 말을 하는 것 같아 입면이 어려움
+
+2021-05 Cognitive impairment
+인지저하 증상이 발생
+Memory - 하려던 행동을 기억하지 못하거나, 전날 있었던 일을 힌트를 주어도 떠올리지 못하는
+short term memory loss 발생
+- 원래 잘 외우던 지인들 전화번호를 기억하지 못함
+Language - 문맹이지만 아들 이름은 쓸 수 있었는데 최근에는 쓰지 못함
+- 말할 때 단어를 빨리 떠올리지 못하여 더듬는 모습을 보이고 이전에 비해 말수가 줄어듦
+Visuospatial - 이전과 크게 다르지 않음, 혼자 길을 잘 찾아다님
+Executive - 집안일 이전처럼 잘 하고 혼자 대중교통 이용도 가능
+ADL - 혼자 위생관리 잘하며 적절한 옷도 잘 골라 입음, 농사일도 유지`,
+        },
+      ]);
+    }
+  }, [threadId]);
+
+  useEffect(() => {
+    setModules([
+      {
+        messageId: 1,
+        moduleName: "Dementia diagnosis",
+        models: [
+          {
+            name: "SPMM",
+            shortDescription: "model for molecule generation",
+            task: "property_to_molecule"
+          }, {
+            name: "DEMO",
+            shortDescription: "demo model",
+            task: "property_to_molecule"
+          }, {
+            name: "DEMO",
+            shortDescription: "demo model",
+            task: "property_to_molecule"
+          }, {
+            name: "DEMO",
+            shortDescription: "demo model",
+            task: "property_to_molecule"
+          }, {
+            name: "DEMO",
+            shortDescription: "demo model",
+            task: "property_to_molecule"
+          }
+        ],
+        summary: "Lorem ipsum dolor sit amet consectetur. Arcu euismod ornare vel ut aliquet et lacus vel. Accumsan ante cursus pellentesque nunc duis. Et blandit malesuada non facilisis. Potenti enim morbi gravida sit. Gravida tortor bibendum orci sit elit in tempus ante accumsan. Luctus eget faucibus congue tempor egestas volutpat interdum viverra."
+      }
+    ])
+  }, [])
+
+  return (
+    <div className="flex flex-col items-center p-12 gap-12">
+      <div className="chatbox-wrapper max-w-[64rem] w-full">
+        {message.map((message) => (
+          <>
+            <ChatBox
+              key={message.messageId}
+              getQuery={() => {}}
+              query={message.message}
+            />
+            <div className="mt-12">
+              {
+                modules.length && <ModuleGroupBox {
+                  ...(modules?.find((module) => module.messageId === message.messageId))
+                } />
+              }
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/components/ChatBox.tsx
+++ src/app/components/ChatBox.tsx
@@ -1,45 +1,97 @@
-import { PaperAirplaneIcon, ArrowUpOnSquareIcon } from "@heroicons/react/24/outline";
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import classNames from "classnames";
+
+import {
+  PaperAirplaneIcon,
+  ArrowUpOnSquareIcon,
+  PencilSquareIcon,
+} from "@heroicons/react/24/outline";
 import Button from "./Button";
 
 type Props = {
   isFileAttachEnabled?: boolean;
   rows?: number;
+  query?: string;
   getQuery: (text: string) => void;
 };
 
 export default function ChatBox({
   isFileAttachEnabled = false,
   rows = 6,
-  getQuery,
+  query = "",
 }: Props) {
+  const [editable, setEditable] = useState(query === "");
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const textAreaAutoResize = (e: any) => {
+    e.target.style.height = "inherit";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+  };
+
+  const textAreaSizeFitToContent = () => { 
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = "inherit";
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+    }
+  }
+
+  useEffect(() => {
+    textAreaSizeFitToContent();
+  }, [editable]);
+
+  const sendQuery = () => {
+    // TODO: send query to server
+  };
+
   return (
-    <div className="flex flex-col border-mint border bg-white rounded-2xl px-8 py-4 w-full shadow-solid shadow-black">
-      <textarea
-        className="w-full resize-none outline-none border-none"
-        placeholder=""
-        rows={rows}
-        onChange={(e) => getQuery(e.target.value)}
-      />
+    <div className="flex flex-col border-mint border bg-white rounded-2xl px-8 pt-6 pb-4 w-full shadow-solid shadow-black leading-7">
+      {editable ? (
+        <textarea
+          className={classNames("w-full resize-none outline-none border-none")}
+          placeholder=""
+          rows={rows}
+          onChange={(e) => {
+            textAreaAutoResize(e);
+          }}
+          ref={textAreaRef}
+        >
+          {query}
+        </textarea>
+      ) : (
+        <div className="w-full resize-none outline-none border-none">
+          <p className="whitespace-pre-line">{query}</p>
+        </div>
+      )}
       <div className="w-full flex justify-between">
-        {
-          isFileAttachEnabled && (
-            <div className="flex items-center text-sm gap-2">
-              <p className="mr-4 font-medium">Files</p>
-              <Button
-                onClick={() => { }}
-                size="sm"
-                color="mint"
-                shadowColor="black"
-              >
-                <ArrowUpOnSquareIcon width="20" />
-                Click to upload
-              </Button>
-              <p>or drag & drop files here</p>
-            </div>
-          )
-        }
-        <div className="rounded-full flex items-center p-3 hover:bg-turquiose hover:text-white transition ease-in-out">
-        <PaperAirplaneIcon width="24" />
+        {isFileAttachEnabled && (
+          <div className="flex items-center text-sm gap-2">
+            <p className="mr-4 font-medium">Files</p>
+            <Button
+              onClick={() => {}}
+              size="sm"
+              color="mint"
+              shadowColor="black"
+            >
+              <ArrowUpOnSquareIcon width="20" />
+              Click to upload
+            </Button>
+            <p>or drag & drop a file here</p>
+          </div>
+        )}
+        <div className="flex-1"></div>
+        <div
+          className="rounded-full flex items-center p-3 hover:bg-turquiose hover:text-white transition ease-in-out cursor-pointer"
+          onClick={() => {
+            setEditable(!editable);
+          }}
+        >
+          {editable ? (
+            <PaperAirplaneIcon width="24" />
+          ) : (
+            <PencilSquareIcon width="24" />
+          )}
         </div>
       </div>
     </div>
