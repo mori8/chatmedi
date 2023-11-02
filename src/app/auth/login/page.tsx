ef59f0db13352612d2b13bcfffd05841bdf48169
mori8 <kaithape@gmail.com>
2023-11-03 01:28:52 +0900

    update: show account infomation in navigation bar

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -23,4 +23,3 @@ export default function page({ }: Props) {
     </div>
   );
 }
-
--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 import { Cog6ToothIcon } from "@heroicons/react/24/outline";
 import { Tooltip } from "react-tooltip";
@@ -12,6 +13,7 @@ type Props = {};
 
 export default function Navigation({ }: Props) {
   const isLogin = usePathname().startsWith("/auth/login");
+  const { data: session } = useSession();
 
   const tabList = [
     {
@@ -23,13 +25,6 @@ export default function Navigation({ }: Props) {
       path: "/documents",
     },
   ];
-
-  const userInfo = {
-    profileImage:
-      "https://camo.githubusercontent.com/021ecb0754c92a2ff6e702ad2e94d1d5863c14c28e40780edc0153a8392a7b89/68747470733a2f2f692e6962622e636f2f5964356a53584b2f726972616b756d612d7073612e706e67",
-    name: "Suyeon Nam",
-    email: "kaithape@gmail.com",
-  };
 
   return (
     !isLogin && <div className="z-10 w-full pt-6 px-12 flex justify-between">
@@ -45,26 +40,26 @@ export default function Navigation({ }: Props) {
           </Link>
         ))}
       </ul>
-      <AccountInfoBox {...userInfo} />
+      {session && <AccountInfoBox {...session.user} />}
     </div>
   );
 }
 
 function AccountInfoBox({
-  profileImage,
+  image,
   name,
   email,
 }: {
-  profileImage: string;
-  name: string;
-  email: string;
+  image?: string | null | undefined;
+  name?: string | null | undefined;
+  email?: string | null | undefined;
 }) {
   return (
     <div className="bg-white rounded-full pl-3 pr-4 py-2 shadow-solid flex items-center gap-6">
       <div className="flex flex-row gap-2 item-center">
         <div className="flex-shrink-0 rounded-full overflow-hidden">
           {/* <Image src={profileImage} width="32" height="32" alt="profile-image" /> */}
-          <img src={profileImage} width="36" height="36" alt="profile-image" />
+          <img src={image || '/user.svg'} width="36" height="36" alt="profile-image" />
         </div>
         <div className="flex-1 flex flex-col justify-evenly">
           <h1 className="text-sm font-medium leading-none">{name}</h1>
@@ -80,6 +75,7 @@ function AccountInfoBox({
           <Cog6ToothIcon width="24" />
         </Link>
       </div>
+      <Tooltip id="settings" place="bottom"/>
     </div>
   );
 }
