4a0727e8d2612f08ff58a2f95676de04396ca5b5
mori8 <kaithape@gmail.com>
2023-11-02 22:02:16 +0900

    update: use jwt & page protection

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,20 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({  
-	providers: [
-  GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID || "", 
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-	  }),
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
   ],
   pages: {
     signIn: "/auth/login",
-  }
+  },
+  session: { strategy: "jwt" },
+  jwt: {
+    maxAge: 60 * 60 * 24 * 30,
+  },
 });
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
