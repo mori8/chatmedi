ddea3588f74ac3cb7e5f054928b9feeda26353d7
mori8 <kaithape@gmail.com>
2023-11-19 00:57:16 +0900

    temporarily remove navigation tab

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -10,36 +10,39 @@ import { Tooltip } from "react-tooltip";
 
 type Props = {};
 
-export default function Navigation({ }: Props) {
+export default function Navigation({}: Props) {
   const isLogin = usePathname().startsWith("/auth/login");
   const { data: session } = useSession();
 
-  const tabList = [
-    {
-      name: "PLAYGROUND",
-      path: "/",
-    },
-    {
-      name: "DOCUMENTS",
-      path: "/documents",
-    },
+  const tabList: {
+    name: string;
+    path: string;
+  }[] = [
+    // {
+    //   name: "PLAYGROUND",
+    //   path: "/",
+    // },
+    // {
+    //   name: "DOCUMENTS",
+    //   path: "/documents",
+    // },
   ];
 
   return (
-    !isLogin && <div className="z-10 w-full pt-6 px-12 flex justify-between">
-      <ul className="flex flex-row gap-6 text-sm">
-        {tabList.map((tab, index) => (
-          <Link href={tab.path} key={index}>
-            <li
-              className="rounded-full hover:bg-white px-6 py-3 flex items-center leading-none"
-            >
-              <span className="font-mono">{tab.name}</span>
-            </li>
-          </Link>
-        ))}
-      </ul>
-      {session && <AccountInfoBox {...session.user} />}
-    </div>
+    !isLogin && (
+      <div className="z-10 w-full pt-6 px-12 flex justify-between">
+        <ul className="flex flex-row gap-6 text-sm">
+          {tabList.map((tab, index) => (
+            <Link href={tab.path} key={index}>
+              <li className="rounded-full hover:bg-white px-6 py-3 flex items-center leading-none">
+                <span className="font-mono">{tab.name}</span>
+              </li>
+            </Link>
+          ))}
+        </ul>
+        {session && <AccountInfoBox {...session.user} />}
+      </div>
+    )
   );
 }
 
@@ -56,7 +59,12 @@ function AccountInfoBox({
     <div className="bg-white rounded-full pl-3 pr-4 py-2 shadow-solid flex items-center gap-6">
       <div className="flex flex-row gap-2 item-center">
         <div className="flex-shrink-0 rounded-full overflow-hidden">
-          <img src={image || '/user.svg'} width="36" height="36" alt="profile-image" />
+          <img
+            src={image || "/user.svg"}
+            width="36"
+            height="36"
+            alt="profile-image"
+          />
         </div>
         <div className="flex-1 flex flex-col justify-evenly">
           <h1 className="text-sm font-medium leading-none">{name}</h1>
@@ -72,7 +80,7 @@ function AccountInfoBox({
           <Cog6ToothIcon width="24" />
         </Link>
       </div>
-      <Tooltip id="settings" place="bottom"/>
+      <Tooltip id="settings" place="bottom" />
     </div>
   );
 }
