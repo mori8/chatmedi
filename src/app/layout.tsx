d2244523d30f5d541948fd7de3ca457e6a21cca2
mori8 <kaithape@gmail.com>
2024-06-24 13:58:10 +0900

    Feat: Logout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import RecoilRootWrapper from "@/context/RecoilRootWrapper";
 import NextAuthContext from "@/context/NextAuthContext";
+import 'react-tooltip/dist/react-tooltip.css'
 import "./globals.css";
 
 const nunito = Nunito({ subsets: ["latin"] });
--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -1,18 +1,22 @@
-'use client';
+"use client";
 
 import React, { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
 import { PlusIcon } from "@heroicons/react/20/solid";
-import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+import {
+  Cog6ToothIcon,
+  ArrowRightStartOnRectangleIcon,
+} from "@heroicons/react/24/outline";
 import { useSession } from "next-auth/react";
 import classNames from "classnames";
-import { fetchUserChats } from "@/lib/redis";
+import { Tooltip } from "react-tooltip";
 
+import { fetchUserChats } from "@/lib/redis";
 
 type Props = {};
 
-export default function ChatSideBar({ }: Props) {
+export default function ChatSideBar({}: Props) {
   const { chatId } = useParams<{ chatId: string }>();
   const { data: session, status } = useSession();
   const user = session?.user;
@@ -35,29 +39,36 @@ export default function ChatSideBar({ }: Props) {
         </div>
         <div>
           <Link href="/chat">
-          <button className="flex items-center gap-1 bg-xanthous text-black py-2 px-3 rounded-2xl font-medium">
-            <PlusIcon width={20} height={20} /> New chat
-          </button>
+            <button className="flex items-center gap-1 bg-xanthous text-black py-2 px-3 rounded-2xl font-medium">
+              <PlusIcon width={20} height={20} /> New chat
+            </button>
           </Link>
         </div>
       </div>
       <div className="flex-1 overflow-auto">
         {status === "loading" ? (
           <div className="animate-pulse">Loading chat history...</div>
+        ) : chatIds.length > 0 ? (
+          <ul>
+            {chatIds.map((_chatId) => (
+              <li
+                key={_chatId}
+                className={classNames(
+                  "py-2 px-3 truncate hover:bg-white hover:bg-opacity-20 rounded-xl",
+                  {
+                    "bg-white bg-opacity-20": _chatId === chatId,
+                  }
+                )}
+              >
+                <a
+                  href={`/chat/${_chatId}`}
+                  className="text-sm font-semibold"
+                >{`${_chatId}`}</a>
+              </li>
+            ))}
+          </ul>
         ) : (
-          chatIds.length > 0 ? (
-            <ul>
-              {chatIds.map((_chatId) => (
-                <li key={_chatId} className={classNames("py-2 px-3 truncate hover:bg-white hover:bg-opacity-20 rounded-xl", {
-                  "bg-white bg-opacity-20": _chatId === chatId,
-                })}>
-                  <a href={`/chat/${_chatId}`} className="text-sm font-semibold">{`${_chatId}`}</a>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No chat history found.</p>
-          )
+          <p>No chat history found.</p>
         )}
       </div>
       <div className="flex-shrink-0 bg-white rounded-2xl text-black flex justify-between items-center px-3 py-2 gap-2">
@@ -73,14 +84,23 @@ export default function ChatSideBar({ }: Props) {
         ) : (
           <>
             <div className="">
-              <img src={user?.image || ""} alt="Profile" className="w-9 h-9 rounded-full" />
+              <img
+                src={user?.image || ""}
+                alt="Profile"
+                className="w-9 h-9 rounded-full"
+              />
             </div>
             <div className="flex-1">
               <p className="leading-none mb-1 font-bold">{user?.name}</p>
-              <p className="text-xs text-slate-400 leading-none">{user?.email}</p>
+              <p className="text-xs text-slate-400 leading-none">
+                {user?.email}
+              </p>
             </div>
             <div>
-              <Cog6ToothIcon width={24} height={24} />
+              <Tooltip id="logout" />
+              <a href="/api/auth/signout" data-tooltip-id="logout" data-tooltip-content="Logout">
+                <ArrowRightStartOnRectangleIcon width={24} height={24} />
+              </a>
             </div>
           </>
         )}
